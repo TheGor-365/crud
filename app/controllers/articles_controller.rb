570e10cffe78e026a8a2d8125f81372ac67334e1
Gor <7495500@gmail.com>
2023-05-24 15:51:47 +0300

    Add articles show

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,25 @@
 class ArticlesController < ApplicationController
+
   def index
     @articles = Article.all.order(created_at: :desc)
+  end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    # debugger
+    @article = Article.new(article_params)
+  end
+
+  def show
+    @article = Article.find(params[:id])
+  end
+
+  private
+
+  def article_params
+    params.require(:article).permit(:title, :body, :author)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   root "articles#index"
 
-  resources :article
+  resources :articles
 end
