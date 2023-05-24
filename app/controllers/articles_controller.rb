bb6bc044e214420f3cace359ce7c38a77efd7a75
Gor <7495500@gmail.com>
2023-05-24 19:17:20 +0300

    Add update and destroy

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,5 @@
 class ArticlesController < ApplicationController
+  befor_action :set_article, only: %i[show edit update destroy]
 
   def index
     @articles = Article.all.order(created_at: :desc)
@@ -11,13 +12,38 @@ class ArticlesController < ApplicationController
   def create
     # debugger
     @article = Article.new(article_params)
+
+    if @article.save
+      redirect_to article_path(@article)
+    else
+      render :new
+    end
   end
 
-  def show
-    @article = Article.find(params[:id])
+  def show; end
+  def edit; end
+
+  def update
+    if @article.update(article_params)
+      redirect_to article_path(@article)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    if @article.destroy
+      redirect_to root_path
+    else
+      redirect_to article_path(@article)
+    end
   end
 
   private
+
+  def set_article
+    @article = Article.find(params[:id])
+  end
 
   def article_params
     params.require(:article).permit(:title, :body, :author)
